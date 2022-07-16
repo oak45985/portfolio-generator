9abd1ff93cbd4ae7c9392096c19f0041187c6a59
oak45985 <oak45985@gmail.com>
2022-07-16 16:26:32 -0500

    created webpage and used modular options within node js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const profileDataArr = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-// console.log(profileDataArgs);
+const pageHTML = generatePage(name, github);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log("=============");
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArr);
+fs.writeFile("./index.html", pageHTML , err => {
+    if(err) throw new err;
+    console.log("Portfolio complete! Checkout index.html to see the output!");
+});
