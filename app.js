2325c135d39cf5096a0944891cf4fc1302dcf6f6
oak45985 <oak45985@gmail.com>
2022-07-17 12:01:37 -0500

    finalized updated template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const fs = requre('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template')
+const generatePage = require('./src/page-template')
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -53,7 +53,8 @@ const promptProject = portfolioData => {
     if (!portfolioData.projects) {
         portfolioData.projects = [];
     }
-        return inquirer.prompt([
+        return inquirer
+        .prompt([
             {
                 type: "input",
                 name: "name",
@@ -125,15 +126,10 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw new Error(err);
+        console.log("Page created! Check out index.html in this directory to see it!");
+        });
     });
-// const fs = require("fs");
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-
-// fs.writeFile("./index.html", pageHTML , err => {
-//     if(err) throw new err;
-//     console.log("Portfolio complete! Checkout index.html to see the output!");
-// });
