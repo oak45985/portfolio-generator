e3f0029baf84d83b378ce79d10458e8185f6e118
oak45985 <oak45985@gmail.com>
2022-07-16 19:36:25 -0500

    updated

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-
-const pageHTML = generatePage(name, github);
+const inquirer = require('inquirer');
 
 
-fs.writeFile("./index.html", pageHTML , err => {
-    if(err) throw new err;
-    console.log("Portfolio complete! Checkout index.html to see the output!");
-});
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    }
+])
+.then(answers => console.log(answers));
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile("./index.html", pageHTML , err => {
+//     if(err) throw new err;
+//     console.log("Portfolio complete! Checkout index.html to see the output!");
+// });
